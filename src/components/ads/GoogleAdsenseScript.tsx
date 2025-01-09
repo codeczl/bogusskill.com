9cb4a6c36443f19cef69cff6af966ca8ccd8a701
codeczl <czhenglong451@gmail.com>
2025-01-09 10:27:20 +0800

    提交说明

--- src/components/ads/GoogleAdsenseScript.tsx
+++ src/components/ads/GoogleAdsenseScript.tsx
@@ -4,20 +4,13 @@ import Script from 'next/script'
 
 const GoogleAdsenseScript = () => {
   return (
-    <>
-      <Script
-        id="adsbygoogle-init"
-        strategy="afterInteractive"
-        async
-        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8919061004428483`}
-        crossOrigin="anonymous"
-      />
-      <Script id="adsbygoogle-load" strategy="afterInteractive">
-        {`
-          (adsbygoogle = window.adsbygoogle || []).push({});
-        `}
-      </Script>
-    </>
+    <Script
+      id="adsbygoogle-init"
+      strategy="afterInteractive"
+      async
+      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8919061004428483`}
+      crossOrigin="anonymous"
+    />
   )
 }
 
